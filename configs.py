6d6e074b51136c7edce6a50eb40a4e08e25da715
KillerSOM <136156372+KillerSOM@users.noreply.github.com>
2024-06-16 23:35:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5297903100"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://killersom72:killersom72@sanjifilestore.x6rhkdi.mongodb.net/?retryWrites=true&w=majority&appName=SanjiFileStore")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001813852666")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001813852666")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
